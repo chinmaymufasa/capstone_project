2523a4e04bb410f039d44c2afec900d8ef12485f
chinmaymufasa <34931442+chinmaymufasa@users.noreply.github.com>
2022-02-25 19:25:48 -0800

    Updated second API key

--- taskit/ios/Runner/AppDelegate.swift
+++ taskit/ios/Runner/AppDelegate.swift
@@ -11,7 +11,7 @@ import GoogleMaps
     GeneratedPluginRegistrant.register(with: self)
 
     // TODO: Add your Google Maps API key
-    GMSServices.provideAPIKey("AIzaSyDl40SkHzE53E80vL3j7345mwSk-8Oh4GI")
+    GMSServices.provideAPIKey("")
 
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
